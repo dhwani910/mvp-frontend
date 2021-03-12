356e1785f1516ad9e7acd7bfebe2d34bdd807853
unknown <pateldhwani59@gmail.com>
2021-03-11 18:59:28 -0800

    created game

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <overview />
+    <upgrades />
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -10,14 +12,52 @@
 </template>
 
 <script>
+import Overview from './components/Overview.vue';
+import Upgrades from './components/Upgrades.vue';
 
 export default {
   name: "app",
+  components: {
+    Overview,
+    Upgrades
+  },
+  methods: {
+    coding(){
+      this.$store.commit('incrementBytes', this.$store.state.bpk);
+
+    },
+    loop(){
+      //Game Loop
+      this.$store.commit('bytesPerSecond');
+      this.levelManager();
+      requestAnimationFrame(this.loop);
+    },
+    levelManager(){
+      if (this.$store.getters.bytesUntilLevelUp <= 0){
+        this.$store.commit('levelUp');
+      }
+    }
+  },
+  created(){
+    this.loop();
+    window.addEventListener('keypress', this.coding);
+  },
+  destroyed(){
+    window.removeEventListener('keypress', this.coding);
+  }
   
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
